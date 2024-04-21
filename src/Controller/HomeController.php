a3ea24bea3e50e904388f24031115caa31c2f24d
Morgane Breton <morgane4478@gmail.com>
2024-04-21 18:36:47 +0200

    DM-29

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Report\MessageReport;
+use App\Entity\Report\PostReport;
+use App\Entity\Report\UserReport;
+use App\Entity\UnbanRequest;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,7 +15,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'home')]
-    public function index(Request $request): Response
+    public function index(EntityManagerInterface $entityManager, Request $request): Response
     {
         $session = $request->getSession();
 
@@ -18,8 +23,17 @@ class HomeController extends AbstractController
             return $this->redirectToRoute('login');
         }
 
+        $userReportsCount = sizeof($entityManager->getRepository(UserReport::class)->findOpenReports());
+        $postReportsCount = sizeof($entityManager->getRepository(PostReport::class)->findOpenReports());
+        $messageReportsCount = sizeof($entityManager->getRepository(MessageReport::class)->findOpenReports());
+        $unbanRequestsCount = sizeof($entityManager->getRepository(UnbanRequest::class)->findOpenRequests());
+
         return $this->render('index.html.twig', [
-            'username' => $session->get('username')
+            'username' => $session->get('username'),
+            'urCount' => $userReportsCount,
+            'prCount' => $postReportsCount,
+            'mrCount' => $messageReportsCount,
+            'ubrCount' => $unbanRequestsCount,
         ]);
     }
 }
